3c18ec171a8d6ad4ca1800fc8f0e19fdce939924
asilbe-admin <asilbe.m00d@gmail.com>
2024-05-19 19:15:19 +0500

    background remover bot

---
+++ bot.py
@@ -0,0 +1,232 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.enums import ParseMode
+from aiogram.filters import CommandStart,Command
+from aiogram import F
+from aiogram.types import Message,InlineKeyboardButton, CallbackQuery
+from data import config
+import asyncio
+import logging
+import sys
+from menucommands.set_bot_commands  import set_default_commands
+from baza.sqlite import Database
+from filters.admin import IsBotAdminFilter
+from filters.check_sub_channel import IsCheckSubChannels
+from buttons import admin_keyboard, removebg
+from aiogram.fsm.context import FSMContext #new
+from states.reklama import Adverts
+from aiogram.utils.keyboard import InlineKeyboardBuilder
+# from buttons.removebg import remove_bg
+from buttons.inlinebuttonss import colors_button
+import time 
+
+ADMINS = config.ADMINS
+TOKEN = config.BOT_TOKEN
+CHANNELS = config.CHANNELS
+
+dp = Dispatcher()
+
+
+@dp.message(CommandStart())
+async def start_command(message:Message):
+    full_name = message.from_user.full_name
+    telegram_id = message.from_user.id
+    try:
+        db.add_user(full_name=full_name,telegram_id=telegram_id) #foydalanuvchi bazaga qo'shildi
+        await message.answer(text="Assalomu alaykum,{full_name}\nBu bot rasm orqa fonini o'chirib beradi. \nBotdan foydalanish uchun rasm yuboring❗️❗️❗️")
+    except:
+        await message.answer(text="Assalomu alaykum, rasm yuboring❗️❗️❗️")
+
+#help commands
+@dp.message(Command("help"))
+async def help_commands(message:Message):
+    await message.answer("Sizga qanday yordam kerak")
+
+
+
+#about commands
+@dp.message(Command("about"))
+async def about_commands(message:Message):
+    await message.answer("Sifat 2024")
+
+
+@dp.message(Command("admin"),IsBotAdminFilter(ADMINS))
+async def is_admin(message:Message):
+    await message.answer(text="Admin menu",reply_markup=admin_keyboard.admin_button)
+
+
+@dp.message(F.text=="Foydalanuvchilar soni",IsBotAdminFilter(ADMINS))
+async def users_count(message:Message):
+    counts = db.count_users()
+    text = f"Botimizda {counts[0]} ta foydalanuvchi bor"
+    await message.answer(text=text)
+
+@dp.message(F.text=="Reklama yuborish",IsBotAdminFilter(ADMINS))
+async def advert_dp(message:Message,state:FSMContext):
+    await state.set_state(Adverts.adverts)
+    await message.answer(text="Reklama yuborishingiz mumkin !")
+
+@dp.message(Adverts.adverts)
+async def send_advert(message:Message,state:FSMContext):
+    
+    message_id = message.message_id
+    from_chat_id = message.from_user.id
+    users = db.all_users_id()
+    count = 0
+    for user in users:
+        try:
+            await bot.copy_message(chat_id=user[0],from_chat_id=from_chat_id,message_id=message_id)
+            count += 1
+        except:
+            pass
+        time.sleep(0.01)
+    
+    await message.answer(f"Reklama {count}ta foydalanuvchiga yuborildi")
+    await state.clear()
+
+
+
+@dp.message(F.photo)
+async def name(message:Message):
+    file_id = message.photo[-1].file_id
+    
+    file = await bot.get_file(file_id)
+    file_path = file.file_path
+    photos_url = f"https://api.telegram.org/file/bot{TOKEN}/{file_path}"
+
+    await message.answer_photo(photo=file_id,reply_markup=colors_button)
+        # await message.answer_photo(photo=types.input_file.BufferedInputFile(rasm,filename="no-bg.png"))
+
+@dp.message()
+async def text_message(message:Message):
+    message.answer("Iltimos, rasm yuboring❗️❗️❗️")
+# black
+@dp.callback_query(F.data=="black")
+async def black_handler(callback:CallbackQuery):
+    await callback.answer("Bu qora")
+    # await callback.message.answer("Qorani bosdingiz")
+    file_id = callback.message.photo[-1].file_id
+    file = await bot.get_file(file_id)
+    file_path = file.file_path
+    photos_url = f"https://api.telegram.org/file/bot{TOKEN}/{file_path}"
+    rasm = removebg.remove_bg(photos_url,"black")
+    await callback.message.answer_photo(photo=types.input_file.BufferedInputFile(rasm,filename="no-bg.png"),reply_markup=colors_button)
+    await callback.message.delete()
+
+# white
+@dp.callback_query(F.data=="white")
+async def white_handler(callback:CallbackQuery):
+    await callback.answer("Bu oq")
+    # await callback.message.answer("Qorani bosdingiz")
+    file_id = callback.message.photo[-1].file_id
+    file = await bot.get_file(file_id)
+    file_path = file.file_path
+    photos_url = f"https://api.telegram.org/file/bot{TOKEN}/{file_path}"
+    rasm = removebg.remove_bg(photos_url,"white")
+    await callback.message.answer_photo(photo=types.input_file.BufferedInputFile(rasm,filename="no-bg.png"),reply_markup=colors_button)
+    await callback.message.delete()
+# yellow
+@dp.callback_query(F.data=="yellow")
+async def yellow_handler(callback:CallbackQuery):
+    await callback.answer("Bu Sariq")
+    # await callback.message.answer("Qorani bosdingiz")
+    file_id = callback.message.photo[-1].file_id
+    file = await bot.get_file(file_id)
+    file_path = file.file_path
+    photos_url = f"https://api.telegram.org/file/bot{TOKEN}/{file_path}"
+    rasm = removebg.remove_bg(photos_url,"yellow")
+    await callback.message.answer_photo(photo=types.input_file.BufferedInputFile(rasm,filename="no-bg.png"),reply_markup=colors_button)
+    await callback.message.delete()
+# red
+@dp.callback_query(F.data=="red")
+async def red_handler(callback:CallbackQuery):
+    await callback.answer("Bu qizil")
+    # await callback.message.answer("Qorani bosdingiz")
+    file_id = callback.message.photo[-1].file_id
+    file = await bot.get_file(file_id)
+    file_path = file.file_path
+    photos_url = f"https://api.telegram.org/file/bot{TOKEN}/{file_path}"
+    rasm = removebg.remove_bg(photos_url,"red")
+    await callback.message.answer_photo(photo=types.input_file.BufferedInputFile(rasm,filename="no-bg.png"),reply_markup=colors_button)
+    await callback.message.delete()
+# blue
+@dp.callback_query(F.data=="blue")
+async def blue_handler(callback:CallbackQuery):
+    await callback.answer("Bu Ko'k")
+    # await callback.message.answer("Qorani bosdingiz")
+    file_id = callback.message.photo[-1].file_id
+    file = await bot.get_file(file_id)
+    file_path = file.file_path
+    photos_url = f"https://api.telegram.org/file/bot{TOKEN}/{file_path}"
+    rasm = removebg.remove_bg(photos_url,"blue")
+    await callback.message.answer_photo(photo=types.input_file.BufferedInputFile(rasm,filename="no-bg.png"),reply_markup=colors_button)
+    await callback.message.delete()
+# green
+@dp.callback_query(F.data=="green")
+async def green_handler(callback:CallbackQuery):
+    await callback.answer("Bu yaxshil")
+    # await callback.message.answer("Qorani bosdingiz")
+    file_id = callback.message.photo[-1].file_id
+    file = await bot.get_file(file_id)
+    file_path = file.file_path
+    photos_url = f"https://api.telegram.org/file/bot{TOKEN}/{file_path}"
+    rasm = removebg.remove_bg(photos_url,"green")
+    await callback.message.answer_photo(photo=types.input_file.BufferedInputFile(rasm,filename="no-bg.png"),reply_markup=colors_button)
+    await callback.message.delete()
+
+
+@dp.message(IsCheckSubChannels())
+async def kanalga_obuna(message:Message):
+    text = ""
+    inline_channel = InlineKeyboardBuilder()
+    for index,channel in enumerate(CHANNELS):
+        ChatInviteLink = await bot.create_chat_invite_link(channel)
+        inline_channel.add(InlineKeyboardButton(text=f"{index+1}-kanal",url=ChatInviteLink.invite_link))
+    inline_channel.adjust(1,repeat=True)
+    button = inline_channel.as_markup()
+    await message.answer(f"{text} kanallarga azo bo'ling",reply_markup=button)
+
+
+
+
+@dp.startup()
+async def on_startup_notify(bot: Bot):
+    for admin in ADMINS:
+        try:
+            await bot.send_message(chat_id=int(admin),text="Bot ishga tushdi")
+        except Exception as err:
+            logging.exception(err)
+
+#bot ishga tushganini xabarini yuborish
+@dp.shutdown()
+async def off_startup_notify(bot: Bot):
+    for admin in ADMINS:
+        try:
+            await bot.send_message(chat_id=int(admin),text="Bot ishdan to'xtadi!")
+        except Exception as err:
+            logging.exception(err)
+
+
+def setup_middlewares(dispatcher: Dispatcher, bot: Bot) -> None:
+    """MIDDLEWARE"""
+    from middlewares.throttling import ThrottlingMiddleware
+
+    # Spamdan himoya qilish uchun klassik ichki o'rta dastur. So'rovlar orasidagi asosiy vaqtlar 0,5 soniya
+    dispatcher.message.middleware(ThrottlingMiddleware(slow_mode_delay=0.5))
+
+
+
+async def main() -> None:
+    global bot,db
+    bot = Bot(TOKEN, parse_mode=ParseMode.HTML)
+    db = Database(path_to_db="main.db")
+    await set_default_commands(bot)
+    await dp.start_polling(bot)
+    setup_middlewares(dispatcher=dp, bot=bot)
+
+
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+
+    asyncio.run(main())
